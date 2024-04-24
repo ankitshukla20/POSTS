01b28f4acec6d757bfa7e5f0a80ae2abbe7f565b
Ankit Shukla <ankit2099shukla@gmail.com>
2024-04-24 10:23:27 +0530

    Add signout button on navbar

--- components/NavButtons.tsx
+++ components/NavButtons.tsx
@@ -3,14 +3,24 @@ import { useUserContext } from "@/lib/userContext";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
+import { auth } from "@/lib/firebase";
 
 export default function NavButtons() {
   const { user, username } = useUserContext();
+
+  const signOut = () => {
+    auth.signOut().then(() => console.log("Logout Success"));
+  };
 
   return (
     <ul>
       {username && (
         <div className="flex gap-6 items-center">
+          <li>
+            <Button variant="secondary" onClick={signOut}>
+              Sign out
+            </Button>
+          </li>
           <li>
             <Link href="/admin">
               <Button>Write Posts</Button>
