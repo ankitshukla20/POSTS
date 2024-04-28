9bc81b402494f6cbf3edc15e99d4e0e33756ffae
Ankit Shukla <ankit2099shukla@gmail.com>
2024-04-28 16:39:20 +0530

    text resize

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -46,7 +46,7 @@ export default async function UserProfilePage({ params: { username } }: Props) {
       <UserDetails user={user} />
       <div className="border bg-slate-50 dark:bg-slate-900 pt-5 pb-2 mb-10 mt-10 rounded-lg">
         <div className="flex justify-center">
-          <h1 className="w-11/12 px-3 mt-2 mb-4 text-3xl font-medium">
+          <h1 className="w-11/12 px-3 mt-2 mb-4 text-2xl font-medium">
             Most recent posts
           </h1>
         </div>
